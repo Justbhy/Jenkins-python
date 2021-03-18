c0968c59e40d1869358f9e164502ecb6ee913842
zhiyong <zhiyongabc@hotmail.com>
2021-03-18 17:37:09 +0800

    v2 commit

--- Jenkins-python/app.py
+++ Jenkins-python/app.py
@@ -10,3 +10,12 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# from flask import Flask
+
+# app = Flask(__name__)
+# @app.route('/')
+# def hello():
+#     return 'Hello World!\n'
+# if __name__ == '__main__':
+#     app.run()
